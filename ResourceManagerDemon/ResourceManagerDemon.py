7b4568eb7a586ade9eb89cbd9609152b9e5c131e
matteo <garato.omicron@gmail.com>
2019-04-26 17:20:11 +0200

    [ADD] - added standard ssl implementation

--- ResourceManagerDemon/ResourceManagerDemon.py
+++ ResourceManagerDemon/ResourceManagerDemon.py
@@ -1,18 +1,23 @@
+import socket
+import ssl
 import daemon
-
 import configparser
-from spam import do_main_program
 import Screen
 import Message
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('/path/to/certchain.pem', '/path/to/private.key')
+
+
 
 MessageQueue = [Message()]
 
 def readMessage(conn):
-        msg = conn.recv()
-        # do something with msg
-        if msg == 'close':
-            conn.close()            
-            listener.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+        sock.bind(('127.0.0.1', 8443))
+        sock.listen(5)
+        with context.wrap_socket(sock, server_side=True) as ssock:
+            conn, addr = ssock.accept()
 
 
 def readConfig():
