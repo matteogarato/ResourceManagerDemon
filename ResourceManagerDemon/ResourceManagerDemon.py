7eda3cf1ba8ce6c1b8ab0733a62fff4ab037b8bb
matteo <garato.omicron@gmail.com>
2019-06-29 17:56:05 +0200

    ADD - function to send message

--- ResourceManagerDemon/ResourceManagerDemon.py
+++ ResourceManagerDemon/ResourceManagerDemon.py
@@ -9,8 +9,6 @@ import SSLMessage as SSLMessageStatic
 import json
 
 
-
-
 MessageQueue = []
 
 def readMessage(bindsocket):
--- ResourceManagerDemon/client.py
+++ ResourceManagerDemon/client.py
@@ -4,7 +4,25 @@ import SSLMessage as sslCommand
 from SSLMessageEncoder import  SSLMessageEncoder as MyEncoder
 import json
 
-def main():
+def sendMessage(line1,line2):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Require a certificate from the server.  We used a self-signed
+        # certificate
+        # so here ca_certs must be the server certificate itself.
+        ssl_sock = ssl.wrap_socket(s,
+                                   ca_certs="server.crt",
+                                   cert_reqs=ssl.CERT_REQUIRED)
+        
+        ssl_sock.connect(('localhost', 10023))
+        params = dict()
+        params['line1'] = line1
+        params['line2'] =line2
+        cmd = sslCommand.SSLMessage('displayMessage',params)
+        converted = serialize(cmd)
+        ssl_sock.write(converted.encode('utf-8'))
+
+def readTemp():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         # Require a certificate from the server.  We used a self-signed
@@ -28,4 +46,4 @@ def serialize(self):
 
 
 if __name__ == '__main__':
-    main()
+    openSocket()
