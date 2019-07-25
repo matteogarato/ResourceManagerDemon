42864c0c6355db85ad33329e1d379a1c808c3bd6
matteo <garato.omicron@gmail.com>
2019-07-25 16:51:41 +0200

    [ADD] - client to class

--- ResourceManagerDemon/client.py
+++ ResourceManagerDemon/client.py
@@ -1,42 +1,40 @@
-import socket
-import ssl
-import SSLMessage as sslCommand
-from SSLMessageEncoder import  SSLMessageEncoder as MyEncoder
-import json
-
-def sendMessage(line1,line2):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ssl_sock = ssl.wrap_socket(s,
-                                   ca_certs="server.crt",
-                                   cert_reqs=ssl.CERT_REQUIRED)
-        
-        ssl_sock.connect(('localhost', 10023))
-        params = dict()
-        params['line1'] = line1
-        params['line2'] =line2
-        cmd = sslCommand.SSLMessage('displayMessage',params)
-        converted = serialize(cmd)
-        ssl_sock.write(converted.encode('utf-8'))
-        ssl_sock.close()
-
-def readTemp():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ssl_sock = ssl.wrap_socket(s,
-                                   ca_certs="server.crt",
-                                   cert_reqs=ssl.CERT_REQUIRED)
-        
-        ssl_sock.connect(('localhost', 10023))
-        params = dict()
-        cmd = sslCommand.SSLMessage('displayMessage',params)
-        converted = serialize(cmd)
-        ssl_sock.write(converted.encode('utf-8'))
-        data = ssl_sock.read()
-        ssl_sock.close()
-
-def serialize(self):    
-    serialized = json.dumps(MyEncoder().encode(self),cls=MyEncoder)
-    return serialized 
-
-
-if __name__ == '__main__':
-    readTemp()
+class Screen(object):
+    import socket
+    import ssl
+    import SSLMessage as sslCommand
+    from SSLMessageEncoder import  SSLMessageEncoder as MyEncoder
+    import json
+    
+    def sendMessage(line1,line2):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ssl_sock = ssl.wrap_socket(s,
+                                       ca_certs="server.crt",
+                                       cert_reqs=ssl.CERT_REQUIRED)
+            
+            ssl_sock.connect(('localhost', 10023))
+            params = dict()
+            params['line1'] = line1
+            params['line2'] =line2
+            cmd = sslCommand.SSLMessage('displayMessage',params)
+            converted = serialize(cmd)
+            ssl_sock.write(converted.encode('utf-8'))
+            ssl_sock.close()
+    
+    def readTemp():
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ssl_sock = ssl.wrap_socket(s,
+                                       ca_certs="server.crt",
+                                       cert_reqs=ssl.CERT_REQUIRED)
+            
+            ssl_sock.connect(('localhost', 10023))
+            params = dict()
+            cmd = sslCommand.SSLMessage('displayMessage',params)
+            converted = serialize(cmd)
+            ssl_sock.write(converted.encode('utf-8'))
+            data = ssl_sock.read()
+            ssl_sock.close()
+    
+    def serialize(self):    
+        serialized = json.dumps(MyEncoder().encode(self),cls=MyEncoder)
+        return serialized 
+    
