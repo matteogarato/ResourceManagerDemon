3366737d9aa1909f4df558ec39c432a91a6a1c60
matteo <garato.omicron@gmail.com>
2019-08-15 08:49:45 +0200

    [ADD] - back to execution

--- ResourceManagerDemon/ResourceManagerDemon.py
+++ ResourceManagerDemon/ResourceManagerDemon.py
@@ -76,6 +76,6 @@ def main():
         messageQueueRemover()
 
 #run the daemon calling main
-with daemon.DaemonContext():
+#with daemon.DaemonContext():
+if __name__ == '__main__':
     main()
-#if __name__ == '__main__':
