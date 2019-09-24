d0c663dca6c78cdce656a7df901f3a6845e9bd05
matteo <garato.omicron@gmail.com>
2019-09-24 18:57:05 +0200

    [ADD] - multi message handling

--- ResourceManagerDemon/Screen.py
+++ ResourceManagerDemon/Screen.py
@@ -19,19 +19,22 @@ class Screen(object):
    lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
    
    def textmessagerecieved(self,line1,line2):
-       self.display = True
-       chardiffLine1 = len(line1) - 16
-       if chardiffLine1 > 0:
-           raise NotImplementedError('Line scroll on multiple line not supported yet')
-       chardiffLine2 = len(line2) - 16
-       if chardiffLine2 > 0:
-           for i in range(0, chardiffLine2 + 1):
-               self.lcd.clear()
-               self.lcd.message("{}\n{}".format((line1).center(16),line2[i:16 + i]))
-               print("rotating:{}\n{}".format((line1).center(16),line2[i:16 + i]))
-               time.sleep(0.6)
-       else:
-           self.lcd.clear()
-           self.lcd.message("{}\n{}".format((line1).center(16),(line2).center(16)))
-       time.sleep(20)
-       self.display = False
+       if(not self.display):
+            self.display = True
+            self.lcd.clear()
+            chardiffLine1 = len(line1) - 16
+            if chardiffLine1 > 0:
+                raise NotImplementedError('Line scroll on multiple line not supported yet')
+            chardiffLine2 = len(line2) - 16
+            if chardiffLine2 > 0:
+                for i in range(0, chardiffLine2 + 1):
+                    self.lcd.clear()
+                    self.lcd.message("{}\n{}".format((line1).center(16),line2[i:16 + i]))
+                    print("rotating:{}\n{}".format((line1).center(16),line2[i:16 + i]))
+                    time.sleep(0.6)
+            else:
+                self.lcd.clear()
+                self.lcd.message("{}\n{}".format((line1).center(16),(line2).center(16)))
+            time.sleep(2)
+            self.lcd.clear()
+            self.display = False
